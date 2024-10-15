71f50bd5f312cb9deecffe2082b82596b0183750
osama ahmed <osamagamil2070@gmail.com>
2024-10-15 20:04:41 +0300

    add profile to development

---
+++ lib/features/sports/presentation/screens/my_profile_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyProfileScreen extends StatelessWidget {
+  const MyProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text("My Profile Screen")),
+    );
+  }
+}
