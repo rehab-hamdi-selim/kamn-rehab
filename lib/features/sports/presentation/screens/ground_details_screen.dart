f7be63aac51314d97a052494f2d94e8f3722fff8
osama ahmed <osamagamil2070@gmail.com>
2024-10-15 19:55:02 +0300

    ground details ui branch init

---
+++ lib/features/sports/presentation/screens/ground_details_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class GroundDetailsScreen extends StatelessWidget {
+  const GroundDetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text("Ground Details")),
+    );
+  }
+}
