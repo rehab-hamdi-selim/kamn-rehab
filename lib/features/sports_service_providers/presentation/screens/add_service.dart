5625c91cda16f151138c0fb27b7ecb8dc7ce520d
osama ahmed <osamagamil2070@gmail.com>
2024-10-17 02:27:03 +0300

    service provider add service branch init

---
+++ lib/features/sports_service_providers/presentation/screens/add_service.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AddServiceScreen extends StatelessWidget {
+  const AddServiceScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Add Service Screen')),
+    );
+  }
+}
