2a2fe50504724a5f3c32987f9ba87bcc121e2b39
ezzeldeen377 <ezzeldeentv20@gmail.com>
2024-10-16 16:29:14 +0300

    finsih ground details screen with all features

--- lib/core/const/constants.dart
+++ lib/core/const/constants.dart
@@ -7,6 +7,10 @@ class Constants {
   ];
 
   static const noConnectionErrorMessage = 'Not connected to a network!';
+  static const directions = 'Dirextions';
+  static const playGroundFeatures = 'Playground Features';
+  static const available = 'Available';
+  static const bookNow = 'Book Now';
 
   static const String googleStoreLink = 'https://newsapi.org/v2/';
   static const String appStoreLink = 'https://newsapi.org/v2/';
--- lib/main.dart
+++ lib/main.dart
@@ -16,13 +16,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: GroundDetailsScreen(),
       ),
-      home: const GroundDetailsScreen(),
     );
   }
 }
