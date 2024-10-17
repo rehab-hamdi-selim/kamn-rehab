ed14f9e50abc1ae63be40823388c64af5887fc90
osama ahmed <osamagamil2070@gmail.com>
2024-10-17 13:13:13 +0300

    brance features/service_providers/service_provider_ground_ui_and_successfully_add_service_ui init

---
+++ lib/features/sports_service_providers/presentation/screens/service_provider_grounds_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ServiceProviderGroundsScreen extends StatelessWidget {
+  const ServiceProviderGroundsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Grounds')),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-
-import 'package:kamn/features/sports/presentation/screens/grounds_screen.dart';
-
+import 'features/sports_service_providers/presentation/screens/service_provider_grounds_screen.dart';
 import 'init_dependencies.dart';
 
 void main() async {
@@ -19,16 +17,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size(375, 812),
-      minTextAdapt: true,
-      splitScreenMode: true,
-      child: MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const GroundsScreen(),
-    );
+        designSize: const Size(375, 812),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          home: const ServiceProviderGroundsScreen(),
+        ));
   }
 }
