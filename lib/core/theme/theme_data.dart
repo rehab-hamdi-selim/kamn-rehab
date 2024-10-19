de495474d8c13e6c164286747a991f007eb33c13
osama ahmed <osamagamil2070@gmail.com>
2024-10-20 02:55:17 +0300

    commit old theme

--- lib/core/theme/theme_data.dart
+++ lib/core/theme/theme_data.dart
@@ -2,17 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:kamn/core/theme/app_pallete.dart';
 
 class AppTheme {
-  // static _border([Color color = AppPallete.borderColor]) =>
-  //     OutlineInputBorder(borderSide: BorderSide(width: 3, color: color));
-  // static final appDarkTheme = ThemeData.dark().copyWith(
-  //     scaffoldBackgroundColor: AppPallete.backgroundColor,
-  //     inputDecorationTheme: InputDecorationTheme(
-  //       contentPadding: const EdgeInsets.all(27),
-  //       border: _border(),
-  //       enabledBorder: _border(),
-  //       focusedBorder: _border(AppPallete.primaryColor),
-  //       errorBorder: _border(AppPallete.errorColor),
-  //     ),
-  //     appBarTheme:
-  //         const AppBarTheme(backgroundColor: AppPallete.backgroundColor));
+//   static _border([Color color = AppPallete.borderColor]) =>
+//       OutlineInputBorder(borderSide: BorderSide(width: 3, color: color));
+//   static final appDarkTheme = ThemeData.dark().copyWith(
+//       scaffoldBackgroundColor: AppPallete.backgroundColor,
+//       inputDecorationTheme: InputDecorationTheme(
+//         contentPadding: const EdgeInsets.all(27),
+//         border: _border(),
+//         enabledBorder: _border(),
+//         focusedBorder: _border(AppPallete.primaryColor),
+//         errorBorder: _border(AppPallete.errorColor),
+//       ),
+//       appBarTheme:
+//           const AppBarTheme(backgroundColor: AppPallete.backgroundColor));
 }
