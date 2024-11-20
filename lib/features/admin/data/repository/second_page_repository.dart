d1a0096d9e1e41c53401cf92612b8e44b2fb1089
Mohammed <mohamedasharf55@gmail.com>
2024-11-20 12:44:33 +0200

    Done Decond Page Repository and Model

---
+++ lib/features/admin/data/repository/second_page_repository.dart
@@ -0,0 +1,21 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:kamn/core/erorr/faliure.dart';
+import 'package:kamn/core/utils/try_and_catch.dart';
+import 'package:kamn/features/admin/data/data_source/second_page_data_source.dart';
+import 'package:kamn/features/admin/data/models/analytics_model.dart';
+
+abstract class SecondPageRepository {
+  Future<Either<Faliure, AnalyticsModel>> getSecondPageData();
+}
+
+class SecondPageRepositoryImpl implements SecondPageRepository {
+  final SecondPageDataSource secondPageDataSource;
+  SecondPageRepositoryImpl({required this.secondPageDataSource});
+  @override
+  Future<Either<Faliure, AnalyticsModel>> getSecondPageData() async {
+    var data = await secondPageDataSource.secondPageDataSource();
+    return executeTryAndCatchForRepository(() async {
+      return AnalyticsModel.fromJson(data);
+    });
+  }
+}
