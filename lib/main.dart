9c5d01381fba24064b52a3da73b9eab7a2d614ed
adham <men3em4@gmail.com>
2024-11-28 13:11:45 +0200

    changing inital route

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        initialRoute: Routes.debitCreditCardPage,
+        initialRoute: Routes.analyticsfirstpage,
         onGenerateRoute: AppRouter.generateRoute,
         // home: FirstAnalyticsPage(),
       ),
