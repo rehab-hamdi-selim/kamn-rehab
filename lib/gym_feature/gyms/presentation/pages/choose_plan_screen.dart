9fdfaf33f6754097473fd37062befc134168780f
ezzeldeen377 <ezzeldeentv20@gmail.com>
2025-02-16 23:04:57 +0200

    init branche

---
+++ lib/gym_feature/gyms/presentation/pages/choose_plan_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChoosePlanScreen extends StatelessWidget {
+  const ChoosePlanScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
