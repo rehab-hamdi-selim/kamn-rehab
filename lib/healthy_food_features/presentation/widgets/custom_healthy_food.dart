cba942001f6908da0d6002a2aa231643ff198ace
ebrahimElsayd <145934999+ebrahimElsayd@users.noreply.github.com>
2025-03-03 15:58:52 +0200

    healthy food feature done model work on it for mostufa

--- lib/healthy_food_features/presentation/widgets/custom_healthy_food.dart
+++ lib/healthy_food_features/presentation/widgets/custom_healthy_food.dart
@@ -1,10 +1,10 @@
-import 'package:flutter/material.dart';
-
-class CustomHealthyFood extends StatelessWidget {
-  const CustomHealthyFood({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
+// import 'package:flutter/material.dart';
+//
+// class CustomHealthyFood extends StatelessWidget {
+//   const CustomHealthyFood({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Placeholder();
+//   }
+// }
