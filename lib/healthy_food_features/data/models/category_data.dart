cebdf9252b82759de2ff29135371ba70be2245df
ebrahimElsayd <145934999+ebrahimElsayd@users.noreply.github.com>
2025-03-03 15:47:29 +0200

    healthy food feature done model

--- lib/healthy_food_features/data/models/category_data.dart
+++ lib/healthy_food_features/data/models/category_data.dart
@@ -24,8 +24,8 @@ class Category {
 List<Category> categories = [
   Category(name: "All", icon: "assets/icons/vegan_1.svg"),
   Category(name: "Vegan", icon: "assets/icons/chickenTest.svg"),
-  Category(name: "Chicken", icon: "assets/icons/vegan_1.svg"),
-  Category(name: "Chicken", icon: "assets/icons/vegan_1.svg"),
+  Category(name: "Chicken", icon: "assets/icons/chickenTest.svg"),
+  Category(name: "Chicken", icon: "assets/icons/chickenTest.svg"),
 ];
 
 
