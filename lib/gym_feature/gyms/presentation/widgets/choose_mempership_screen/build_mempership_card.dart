5f7be909c24791ab19935b22bcc857b51e65803b
ezzeldeen377 <ezzeldeentv20@gmail.com>
2025-03-05 16:46:52 +0200

    handle ui and card background

---
+++ lib/gym_feature/gyms/presentation/widgets/choose_mempership_screen/build_mempership_card.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:kamn/core/helpers/spacer.dart';
+import 'package:kamn/core/theme/app_pallete.dart';
+import 'package:kamn/core/theme/style.dart';
+import 'package:kamn/gym_feature/gyms/data/models/plan_model.dart';
+
+class BuildMempershipCard extends StatelessWidget {
+  const BuildMempershipCard({
+    super.key,
+    required this.plan,
+  });
+  
+  final PlanModel plan;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: AppPallete.lgGreyColor, width: 2.w),
+        image: const DecorationImage(
+          image: AssetImage('assets/images/background.png'),
+          fit: BoxFit.fill,
+        ),
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16.r),
+      ),
+      padding: EdgeInsets.all(16.w),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(plan.subtitle,
+                  style: TextStyles.fontCircularSpotify12BlackRegular),
+              Text(plan.title, style: TextStyles.fontCircularSpotify16BlackMedium),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 8.w, vertical: 4.h),
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(12.r),
+                ),
+                child: Text(plan.discount,
+                    style: TextStyle(
+                        fontFamily: "CircularSpotify",
+                        fontWeight: FontWeight.w900,
+                        color: Colors.white,
+                        fontSize: 12.sp)),
+              ),
+            ],
+          ),
+          SizedBox(height: 8.h),
+          Row(
+            children: [
+              Text(plan.pricelineThrough,
+                  style: TextStyles.fontCircularSpotify10BlackMedium.copyWith(
+                    decoration: TextDecoration.lineThrough,
+                  )),
+              horizontalSpace(10.w),
+              const Icon(
+                size: 12,
+                Icons.arrow_forward,
+                color: AppPallete.accentBlackColor2,
+              ),
+              horizontalSpace(12.w),
+              Text(plan.price,
+                  style: TextStyles.fontCircularSpotify20AccentBlackMedium),
+            ],
+          ),
+          SizedBox(height: 16.h),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: plan.features
+                .map((feature) => Padding(
+                      padding: EdgeInsets.only(bottom: 8.h),
+                      child: Text(feature,
+                          style: TextStyles
+                              .fontCircularSpotify12BlackRegular),
+                    ))
+                .toList(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+ 
+
