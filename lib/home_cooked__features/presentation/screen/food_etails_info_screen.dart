f9a2ab764ae72ef29ca32eb89be0096970bc0ad7
ebrahimElsayd <145934999+ebrahimElsayd@users.noreply.github.com>
2025-03-12 01:08:08 +0200

    you can work on it mafdy

---
+++ lib/home_cooked__features/presentation/screen/food_etails_info_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FoodDetailsInfoScreen extends StatelessWidget {
+  const FoodDetailsInfoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
