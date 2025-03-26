526f5bd4771f5a22542670a98b91e6e2cbc243fa
ezzeldeen377 <ezzeldeentv20@gmail.com>
2025-03-26 22:36:58 +0200

    finish merge

---
+++ lib/gym_feature/add_gym/presentation/screens/membership_offer_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MembershipOfferScreen extends StatelessWidget {
+  const MembershipOfferScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
