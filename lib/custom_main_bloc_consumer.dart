cd2fe030aab266356abde60849b70031d6125514
mena <menasedky22@gmail.com>
2025-03-29 16:35:00 +0200

    add membership offer screen Mena

--- lib/custom_main_bloc_consumer.dart
+++ lib/custom_main_bloc_consumer.dart
@@ -6,17 +6,10 @@ import 'package:kamn/core/routing/app_router.dart';
 import 'package:kamn/core/theme/app_pallete.dart';
 import 'package:kamn/core/utils/show_snack_bar.dart';
 import 'package:kamn/gym_feature/add_gym/presentation/cubits/add_gym/add_gym_cubit.dart';
-import 'package:kamn/gym_feature/add_gym/presentation/screens/add_gym_screen.dart';
 import 'package:kamn/gym_feature/add_gym/presentation/screens/membership_offer_screen.dart';
-import 'package:kamn/gym_feature/gyms/presentation/pages/gyms_screen.dart';
-import 'package:kamn/gym_feature/gyms/presentation/screen/choose_plan_screen.dart';
 import 'package:kamn/gym_feature/gyms/presentation/screen/track_gym_request_submission_screen.dart';
-import 'package:kamn/playground_feature/authentication/presentation/cubits/sign_in_cubit/sign_in_cubit.dart';
-import 'package:kamn/playground_feature/authentication/presentation/screens/on_boarding_screen.dart';
 import 'core/common/cubit/app_user/app_user_cubit.dart';
 import 'core/common/widget/main_loader.dart';
-import 'core/di/di.dart';
-import 'playground_feature/authentication/presentation/screens/sign_in_screen.dart';
 
 class CustomMainBlocConsumer extends StatelessWidget {
   const CustomMainBlocConsumer({super.key});
